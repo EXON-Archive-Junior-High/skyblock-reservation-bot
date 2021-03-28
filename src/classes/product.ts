3bc990ea16e7418abee531cf07cc0edb7300a8c9
시혁 박 <exon.indiv@gmail.com>
2021-03-28 22:01:54 +0900

    Add: Products

--- src/classes/product.ts
+++ src/classes/product.ts
@@ -1,6 +1,53 @@
-class Product {
-    constructor() {}
+export default class Product {
+    constructor(
+        uuid: string,
+        auctioneer: string,
+        profile_id: string,
+        coop: string[],
+        start: number,
+        end: number,
+        item_name: string,
+        item_lore: string,
+        extra: string,
+        category: string,
+        tier: string,
+        starting_bid: number,
+        item_bytes: string,
+        claimed: boolean,
+        claimed_bidders: string[],
+        highest_bid_amount: number,
+        bids: [
+            {
+              auction_id: string,
+              bidder: string,
+              profile_id: string,
+              amount: number,
+              timestamp: number
+            }
+        ],
+        isBin: boolean
+    ) {
+        this.uuid = uuid
+        this.auctioneer = auctioneer
+        this.profile_id = profile_id
+        this.coop = coop
+        this.start = start
+        this.end = end
+        this.item_name = item_name
+        this.item_lore = item_lore
+        this.extra = extra
+        this.category = category
+        this.tier = tier
+        this.starting_bid = starting_bid
+        this.item_bytes = item_bytes
+        this.claimed = claimed
+        this.claimed_bidders = claimed_bidders
+        this.highest_bid_amount = highest_bid_amount
+        this.bids = bids
+        this.isBin = isBin
+    }
 
+    uuid: string
     auctioneer: string
     profile_id: string
     coop: string[]
@@ -25,5 +72,5 @@ class Product {
           timestamp: number
         }
     ]
-    bin: boolean
+    isBin: boolean
 }
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,33 @@
 import fetch from 'node-fetch'
 
+import Product from './classes/product'
+
 const get = async (str: string) => await (await fetch(str)).json()
 
 async function main() {
-    const a = await get('https://api.hypixel.net/skyblock/auctions')
-    console.log(a)
+    const data = await get('https://api.hypixel.net/skyblock/auctions')
+    if (!data.success) return
+    let products: Product[]
+    for (let i = 0; i < data.auctions.length; i++) products.push(new Product(
+        data.auctions.uuid,
+        data.auctions.auctioneer,
+        data.auctions.profile_id,
+        data.auctions.coop,
+        data.auctions.start,
+        data.auctions.end,
+        data.auctions.item_name,
+        data.auctions.item_lore,
+        data.auctions.extra,
+        data.auctions.category,
+        data.auctions.tier,
+        data.auctions.starting_bid,
+        data.auctions.item_bytes,
+        data.auctions.claimed,
+        data.auctions.clasimed_bidders,
+        data.auctions.highest_bid_amount,
+        data.auctions.bids,
+        data.auctions.bin
+    ))
 }
 
 main()
