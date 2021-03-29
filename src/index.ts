8fb180f28a43fd8487f2ebf74237a194087c764b
시혁 박 <exon.indiv@gmail.com>
2021-03-29 22:18:51 +0900

    Add: removeColor

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,37 @@
 import fetch from 'node-fetch'
 
 import Product from './classes/product'
+import removeColor from './utils/removeColor'
 
 const get = async (str: string) => await (await fetch(str)).json()
 
 async function main() {
     const data = await get('https://api.hypixel.net/skyblock/auctions')
     if (!data.success) return
-    let products: Product[]
-    for (let i = 0; i < data.auctions.length; i++) products.push(new Product(
-        data.auctions.uuid,
-        data.auctions.auctioneer,
-        data.auctions.profile_id,
-        data.auctions.coop,
-        data.auctions.start,
-        data.auctions.end,
-        data.auctions.item_name,
-        data.auctions.item_lore,
-        data.auctions.extra,
-        data.auctions.category,
-        data.auctions.tier,
-        data.auctions.starting_bid,
-        data.auctions.item_bytes,
-        data.auctions.claimed,
-        data.auctions.clasimed_bidders,
-        data.auctions.highest_bid_amount,
-        data.auctions.bids,
-        data.auctions.bin
-    ))
+    let products: Product[] = new Array<Product>()
+    for (let i = 0; i < data.auctions.length; i++) {
+        products.push(new Product(
+            data.auctions[i].uuid,
+            data.auctions[i].auctioneer,
+            data.auctions[i].profile_id,
+            data.auctions[i].coop,
+            data.auctions[i].start,
+            data.auctions[i].end,
+            data.auctions[i].item_name,
+            removeColor(data.auctions[i].item_lore),
+            data.auctions[i].extra,
+            data.auctions[i].category,
+            data.auctions[i].tier,
+            data.auctions[i].starting_bid,
+            data.auctions[i].item_bytes,
+            data.auctions[i].claimed,
+            data.auctions[i].clasimed_bidders,
+            data.auctions[i].highest_bid_amount,
+            data.auctions[i].bids,
+            data.auctions[i].bin
+        ))
+        console.log(removeColor(data.auctions[i].item_lore))
+    }
 }
 
 main()
