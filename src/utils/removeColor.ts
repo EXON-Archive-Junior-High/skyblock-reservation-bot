a8365884e318063b60463c221570471ef5775bf4
시혁 박 <exon.indiv@gmail.com>
2021-03-30 21:35:03 +0900

    Add: removeColor

--- src/utils/removeColor.ts
+++ src/utils/removeColor.ts
@@ -1,6 +1,6 @@
 export default function removeColor(text: string): string {
     let result: string = text
     const colorCode: string[] = ['§0', '§1', '§2', '§3', '§4', '§5', '§6', '§7', '§8', '§9', '§a', '§b', '§c', '§d', '§e', '§f']
-    for (let i = 0; i < colorCode.length; i++) result.replace(`/${colorCode[i]}/g`, '')
+    for (let i = 0; i < colorCode.length; i++) result = result.replace(new RegExp(colorCode[i], 'gi'), '')
     return result
 }
