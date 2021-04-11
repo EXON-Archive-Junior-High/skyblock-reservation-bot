e9b4f053b46e535dd565ed73c362f6ae3592e82d
시혁 박 <exon.indiv@gmail.com>
2021-04-11 20:10:54 +0900

    getitemcolor

---
+++ src/utils/getItemColor.ts
@@ -0,0 +1,18 @@
+export function getItemColor(tier: string): number {
+    switch (tier) {
+        case 'COMMON':
+            return 0xffffff
+        case 'UNCOMMON':
+            return 0x55ff55
+        case 'RARE':
+            return 0x5555ff
+        case 'EPIC':
+            return 0xaa00aa
+        case 'LEGENDARY':
+            return 0xffaa00
+        case 'MYSTIC':
+            return 0xFF5555
+        default:
+            return 0x000000
+    }
+}
--- src/utils/product2embed.ts
+++ src/utils/product2embed.ts
@@ -1,6 +1,29 @@
 import { MessageEmbed } from 'discord.js'
 import Product from '../classes/Product'
+import getItemColor from './getItemColor'
 
 export default function product2embed(products: Product[]): MessageEmbed {
-    
+    const Embeds: MessageEmbed[] = new Array<MessageEmbed>()
+    let i = 0
+    products.forEach((product) => {
+        const presentBid: number = product.bids.length ? product.bids[product.bids.length - 1] : product.starting_bid
+        Embeds[i] = new MessageEmbed({
+            title: product.item_name,
+            author: {
+
+            },
+            color: getItemColor(product.tier),
+            fields: [
+                {
+                    name: product.item_name,
+                    value: product.item_lore
+                }
+            ],
+            timestamp: new Date(),
+            footer: {
+                text: presentBid
+            }
+        })
+        i++
+    })
 }
