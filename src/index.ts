1e147b0fd25b6488cdd4dadbe85bbe44df23eabf
시혁 박 <exon.indiv@gmail.com>
2021-04-20 21:48:59 +0900

    Add: Failed, Success

--- src/index.ts
+++ src/index.ts
@@ -62,16 +62,18 @@ client.on('message', async (msg) => {
         const isBin = getBin(args[3].toLowerCase())
 
         await db('channels').insert({ user: msg.author.id, channel_id: msg.channel.id, item_name: args[1], item_price: price, item_bin: isBin})
-        msg.channel.send(new MessageEmbed({title: '✅ Success', description: '"' + args[1] + '" Successfully registered'}))
+        msg.channel.send(new MessageEmbed({title: '✅ Success', description: '"' + args[1] + '" Successfully registered', color: 0x00FF00 }))
     } else if (args[0] === '목록') {
         const rows = await db('channels').select('*').where('user', msg.author.id)
-        let embed: MessageEmbed = new MessageEmbed({ title: 'Item List' })
+        let embed: MessageEmbed = new MessageEmbed({ title: 'Item List', color: 0x00FF00 })
         rows.forEach((row: any) => {
             embed.addField(row.item_name, ':coin:: ' + row.item_price + '\n🛒: ' + getBin(row.item_bin))
         })
         msg.channel.send(embed)
     } else if (args[0] === '삭제') {
-        await db('channels').where({ user: msg.author.id, item_name: args[1] })
+        const isSuccess = await db('channels').where({ user: msg.author.id, item_name: args[1] }).del()
+        if (isSuccess) msg.channel.send(new MessageEmbed({ title: '✅ Success', description: '"' + args[1] + '" Deleted successfully', color: 0x00FF00 }))
+        else msg.channel.send(new MessageEmbed({ title: '❎ Failed', description: 'failed', color: 0xFF0000}))
     }
 })
 
